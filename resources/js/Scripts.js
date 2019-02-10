7dc97077c7e591773343c58c5ee636e479de5b88
riannajane <47402705+riannajane@users.noreply.github.com>
2019-02-10 14:05:46 +1100

    Navigation and Fav Icon

--- resources/js/Scripts.js
+++ resources/js/Scripts.js
@@ -1,2 +1,15 @@
 cssVars();
 
+//open mobile nav 
+
+$('.nav-prompt').click(function () {
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+        $('.nav-inner').attr('aria-expanded', 'true');
+
+    } else {
+
+        $('.nav-inner').attr('aria-expanded', 'false');
+    }
+})
+
+ 
